89da01d0b95f9bf2e319edeacf7a0c0ee1b35ea4
Ian Wootten <3598161+iwootten@users.noreply.github.com>
2022-11-10 23:00:49 +0000

    Use markdownify to estimate markdown

--- get_revue_issues.py
+++ get_revue_issues.py
@@ -3,19 +3,29 @@ from datetime import datetime
 
 import requests
 from slugify import slugify
+from markdownify import markdownify as md
 
-REVUE_KEY = os.getenv('REVUE_API_KEY')
+REVUE_KEY = os.getenv("REVUE_API_KEY")
 
-r = requests.get('https://www.getrevue.co/api/v2/issues', headers={
-    "Authorization": f"Bearer {REVUE_KEY}"
-})
+r = requests.get(
+    "https://www.getrevue.co/api/v2/issues",
+    headers={"Authorization": f"Bearer {REVUE_KEY}"},
+)
 
-if not os.path.exists("issues"):
-    os.makedirs("issues")
+if not os.path.exists("issues/html"):
+    os.makedirs("issues/html")
+
+if not os.path.exists("issues/markdown"):
+    os.makedirs("issues/markdown")
 
 for issue in r.json():
     sent_at = datetime.fromisoformat(issue["sent_at"])
     slug = slugify(f"{sent_at.date()}-{issue['title']}")
 
-    with open(f"issues/{slug}.html", "w+") as issue_file:
-        issue_file.write(issue['html'])
+    with open(f"issues/html/{slug}.html", "w+") as issue_file, open(
+        f"issues/markdown/{slug}.md", "w+"
+    ) as markdown_file:
+        markdown = md(issue["html"])
+
+        issue_file.write(issue["html"])
+        markdown_file.write(markdown)
